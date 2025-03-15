bbbf80b37f826ff43beb40d5332015f4957fba8f
Sathvik Kamegaonkar <sathvik7593@gmail.com>
2025-03-15 23:24:08 +0530

    Resolved merge conflicts

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans} from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "Summmaire",
+  description: "Summaire is an app for summarizing pdfs ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`font-sans ${fontSans.variable } antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
