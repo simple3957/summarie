181c5a31909cdd95270fac9d1a5da2eeb2bdb503
Sathvik Kamegaonkar <sathvik7593@gmail.com>
2025-03-21 20:00:13 +0530

    Added authorization to the website

---
+++ app/(logged-in)/upload/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Upload</h1>
+      <p>This is the Upload page.</p>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Source_Sans_3 as FontSans} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/header";
 import Footer from "@/components/common/footer";
+import { ClerkProvider } from "@clerk/nextjs";  
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body >
         <div className="relative flex flex-col min-h-screen ">
@@ -32,5 +34,6 @@ export default function RootLayout({
       </body>
       
     </html>
+    </ClerkProvider>
   );
 }
